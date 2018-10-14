087ec527942d132741351855bf70256c97bfc6e6
viraj lakshitha <virajlakshitha39@gmail.com>
2018-10-14 22:20:36 +0530

    Added an icon to my account

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,20 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { ViewChild } from '@angular/core';
+import { Slides } from 'ionic-angular';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
-
+  @ViewChild(Slides) slides: Slides;
   constructor(public navCtrl: NavController) {
 
+  }
+
+  goToSlide() {
+    this.slides.slideNext(2, true);
   }
 
 }
